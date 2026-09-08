b64e230836640e0906f89f98fadc5a05a9e72aa1
Brysonlakes <nathicibi2@gmail.com>
2026-09-08 20:57:55 +0200

    Set NODE_TLS_REJECT_UNAUTHORIZED to '0'
    Enable TLS certificate rejection for development.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-require('dotenv').config();
+
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
